60b163381a65bbfa47d564aa19a861008ea94ccc
fb0801 <fbhatti0801@gmail.com>
2023-08-11 14:54:28 +0100

    projectiles and collisions

--- main.py
+++ main.py
@@ -13,12 +13,15 @@ BG = pygame.transform.scale(pygame.image.load('bg.jpeg'), (WIDTH, HEIGHT))
 PLAYER_WIDTH = 40
 PLAYER_HEIGHT =  60
 PLAYER_VEL = 5
+
 STAR_WIDTH = 10
 STAR_HEIGHT = 20
+STAR_VEL = 3
+
 
 FONT = pygame.font.SysFont('comicsans', 30)
 
-def draw(player, elapsed_time):
+def draw(player, elapsed_time, stars):
     WIN.blit(BG, (0,0))
 
     time_text= FONT.render(f'Time:{round(elapsed_time)}s',1, 'white')
@@ -41,6 +44,7 @@ def main():
     star_count = 0
 
     stars= []
+    hit = False
 
     while run:
         star_count += clock.tick(60)
@@ -51,7 +55,7 @@ def main():
                 star_x = random.randint(0, WIDTH - STAR_WIDTH)
                 star = pygame.Rect(star_x, STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
                 stars.append()(star)
-                
+
             star_add_increment = max(200, star_add_increment - 50)
             star_count = 0
 
@@ -66,8 +70,18 @@ def main():
         if keys[pygame.K_RIGHT]and player.x + PLAYER_VEL + player.width <= WIDTH:
             player.x += PLAYER_VEL
 
+        for star in stars[:]:
+            #check if player hit by start
+            star.y += STAR_VEL 
+            if star.y > HEIGHT:
+                stars.remove(star)
+            elif star.y + star.height >= player.y and star.colliderect(player):
+                stars.remove(star)
+                hit = True
+                break
 
-        draw(player, elapsed_time)
+
+        draw(player, elapsed_time, stars)
 
     pygame.quit()
 
